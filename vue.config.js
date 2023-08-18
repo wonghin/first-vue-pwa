4a790c42d49c423990bf6c95fa3bc7b8f872e3b2
Andy <tkp203145@gmail.com>
2023-08-18 08:56:11 +0800

    fix: vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -10,11 +10,7 @@ module.exports = defineConfig({
 
     // configure the workbox plugin
     workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      swSrc: 'dev/sw.js',
-      // ...other Workbox options...
-    },
+
     iconPaths: {
       faviconSVG: 'img/icons/favicon.svg',
       // faviconSVG: 'img/icons/icon-192x192.png',

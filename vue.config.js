b9c1b460556ae0cad0c674cb3f6993f650961e98
Andy <tkp203145@gmail.com>
2023-08-18 09:08:31 +0800

    fix: vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -8,30 +8,6 @@ module.exports = defineConfig({
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
 
-    // configure the workbox plugin
-    workboxPluginMode: 'InjectManifest',
-
-    iconPaths: {
-      faviconSVG: 'img/icons/favicon.svg',
-      // faviconSVG: 'img/icons/icon-192x192.png',
-      favicon32: 'img/icons/favicon-32x32.png',
-      favicon16: 'img/icons/favicon-16x16.png',
-      appleTouchIcon: 'img/icons/apple-touch-icon-152x152.png',
-      maskIcon: 'img/icons/safari-pinned-tab.svg',
-      // maskIcon: 'img/icons/icon-192x192.png',
-      msTileImage: 'img/icons/msapplication-icon-144x144.png'
-    },
-    // icons: [
-    //   {
-    //     src: "img/icons/icon-192x192.png",
-    //     type: "image/png",
-    //     sizes: "192x192"
-    //   },
-    //   {
-    //     src: "img/icons/icon-512x512.png",
-    //     type: "image/png",
-    //     sizes: "512x512"
-    //   }
-    // ],
   }
-})
+}
+)
